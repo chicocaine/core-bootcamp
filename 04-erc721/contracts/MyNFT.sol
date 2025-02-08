b4032250aded43af078a47dadaf778d103ff0ea2
chicocaine <eduardanthony1208@gmail.com>
2025-02-08 16:51:18 +0800

    y e s

--- 04-erc721/contracts/MyNFT.sol
+++ 04-erc721/contracts/MyNFT.sol
@@ -7,16 +7,16 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract MyNFT is ERC721URIStorage {
-    uint256 tokenId;
+    uint256 tokenId = 1;
 
-    constructor() ERC721("MyERC721Token", "MET") {}
+    constructor() ERC721("chicocaine", "CHICO") {}
 
     function mint() external {
-        // _safeMint(msg.sender, tokenId);
-        // _setTokenURI(
-        //     tokenId,
-        //     "https://path/to/metadata-uri"
-        // );
-        // tokenId++;
+        _safeMint(msg.sender, tokenId);
+        _setTokenURI(
+            tokenId,
+            "https://gist.githubusercontent.com/chicocaine/8dc50fe3cb9584ba9078d00f094d4faa/raw/364a0667a4eb81b8910d994c3c59b7b688ee6241/metadata.json"
+        );
+        tokenId++;
     }
 }
